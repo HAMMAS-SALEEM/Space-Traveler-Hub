d3b02e413e0196b1c83f1be3c2a6c08a121cd1b5
HAMMAS BIN SALEEM <hammassaleem376@gmail.com>
2022-02-28 18:40:33 +0500

    Fix Linters errors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Nav />
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import planet from './assets/planet.png';
 
 const Nav = () => (
-  <div className='nav-container'>
-    <img src={planet} alt='planet' />
-    <ul>
-      <Link></Link>
-      <Link></Link>
-      <Link></Link>
-    </ul>
-  </div>
+  <nav className="nav-container">
+    <img src={planet} alt="planet" />
+    <h1>Working</h1>
+  </nav>
 );
 export default Nav;
