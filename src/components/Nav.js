48351fe0ec60edca7cc26c40694becfe957d8fa0
mrparkersson <amoparkerprince78@gmail.com>
2022-02-28 14:27:02 +0000

    Add active style to links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import planet from './assets/planet.png';
 
 const Nav = () => (
   <nav className="nav-container">
     <img src={planet} alt="planet" />
-    <Link to="/">Home</Link>
-    <Link to="/profile">Profile</Link>
+    <NavLink
+      exact
+      to="/"
+      className="main-nav-active"
+      style={({ isActive }) => (isActive ? { color: 'red' } : { color: 'blue' })}
+    >
+      Home
+    </NavLink>
+    <NavLink
+      exact
+      to="/profile"
+      style={({ isActive }) => (isActive ? { color: 'red' } : { color: 'blue' })}
+    >
+      Profile
+    </NavLink>
   </nav>
 );
 export default Nav;
