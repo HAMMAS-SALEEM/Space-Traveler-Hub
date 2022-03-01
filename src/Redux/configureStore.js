2f79641888516d1438d2d591a9949d06511fb8a3
mrparkersson <amoparkerprince78@gmail.com>
2022-03-01 10:12:47 +0000

    Fetch rocket data

---
+++ src/Redux/configureStore.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import rocketReducer from './Rocket/rocket';
+
+const reducer = combineReducers({
+  rocketReducer,
+});
+const store = createStore(reducer, applyMiddleware(logger));
+export default store;
--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,4 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAPI } from '../Redux/Rocket/rocket';
 
-const Rocket = () => <h1>This is the Rocket page</h1>;
-export default Rocket;
+const Mission = () => {
+  const store = useSelector((state) => state.rocketReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    fetch('https://api.spacexdata.com/v3/rockets')
+      .then((res) => res.json())
+      .then((json) => {
+        if (store.length === 0) {
+          dispatch(getAPI(json));
+        }
+      });
+  }, []);
+  return (
+    <ul className="rocket-container">
+      {store.map((rocket) => (
+        <li key={rocket.id}>
+          <ul>
+            <li>{rocket.rocket_name}</li>
+            <li>
+              <img src={rocket.flickr_images[0]} alt="" />
+            </li>
+          </ul>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default Mission;
