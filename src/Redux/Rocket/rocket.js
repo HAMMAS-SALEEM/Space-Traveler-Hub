4247e69bc6b5cfe4b0a34db196e528a9675d4fe6
HAMMAS BIN SALEEM <hammassaleem376@gmail.com>
2022-03-02 13:30:39 +0500

    Added Rocket Reservation

--- src/Redux/Rocket/rocket.js
+++ src/Redux/Rocket/rocket.js
@@ -1,13 +1,28 @@
 const GET_API = 'data/rockets/GET_API';
+const JOIN_ROCKET = 'data/rockets/JOIN_ROCKET';
+
 export const getAPI = (playload) => ({
   type: GET_API,
   playload,
 });
+
+export const joinRocket = (payload) => ({
+  type: JOIN_ROCKET,
+  payload,
+});
+
 const initialState = [];
 const rocketReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_API:
       return [...state, ...action.playload];
+    case JOIN_ROCKET:
+      return state.map((rocket) => {
+        if (rocket.id === action.payload.id) {
+          return { ...rocket, reserved: !rocket.reserved };
+        }
+        return rocket;
+      });
     default:
       return state;
   }
--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useSelector, useDispatch } from 'react-redux';
-import { getAPI } from '../Redux/Rocket/rocket';
+import { getAPI, joinRocket } from '../Redux/Rocket/rocket';
 
 const Mission = () => {
   const store = useSelector((state) => state.rocketReducer);
   const dispatch = useDispatch();
+  const joinRocketHandler = (e) => {
+    const { id } = e.target;
+    dispatch(joinRocket({ id }));
+  };
   useEffect(() => {
     fetch('https://api.spacexdata.com/v3/rockets')
       .then((res) => res.json())
@@ -30,6 +34,7 @@ const Mission = () => {
                 className="rocket-image"
               />
             </li>
+            <li><button id={rocket.id} type="button" onClick={joinRocketHandler}>{rocket.reserved ? 'Cancel Reservation' : 'Reserve'}</button></li>
           </ul>
         </li>
       ))}
