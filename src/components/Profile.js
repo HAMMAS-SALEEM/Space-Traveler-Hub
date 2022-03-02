df6bd5b1f24b27c6400893251f779bbaa92d1fbb
HAMMAS BIN SALEEM <hammassaleem376@gmail.com>
2022-03-02 14:34:00 +0500

    Added Proflie Data

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,25 +6,27 @@ const Profile = () => {
   const mission = useSelector((state) => state.missionReducer);
   return (
     <section className="nav-container">
-      <ul className="rocket-container">
-        {rocket.map(
-          <li>
-            <ul>
-              <li></li>
-              <li></li>
-            </ul>
-          </li>
-        )}
+      <ul className="mission-container">
+        <h2>Missions</h2>
+        {mission.map((mission) => {
+          if (mission.reserved === true) {
+            return (
+              <li key={mission.mission_id}>{mission.mission_name}</li>
+            );
+          }
+          return null;
+        })}
       </ul>
       <ul className="rocket-container">
-        {mission.map(
-          <li>
-            <ul>
-              <li></li>
-              <li></li>
-            </ul>
-          </li>
-        )}
+        <h2>Rockets</h2>
+        {rocket.map((rocket) => {
+          if (rocket.reserved === true) {
+            return (
+              <li key={rocket.rocket_id}>{rocket.rocket_name}</li>
+            );
+          }
+          return null;
+        })}
       </ul>
     </section>
   );
